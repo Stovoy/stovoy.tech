9ae9234d052ba5ded2a735521be2d652fd214d47
Steve Mostovoy <stevemostovoysm@gmail.com>
2018-09-20 21:09:38 -0700

    Change build script.

--- build.sh
+++ build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash -e
 
-(
-    docker run \
-        -v $(pwd)/src:/app/src \
-        -v $(pwd)/.cargo-cache:/usr/local/cargo/registry \
-        -v $(pwd)/target:/app/target \
-        -v $(pwd)/Cargo.toml:/app/Cargo.toml \
-        -v $(pwd)/Cargo.lock:/app/Cargo.lock \
-        --entrypoint bash \
-        rustlang/rust:nightly \
-        -c "cd /app && cargo build"
+docker run \
+    -it \
+    -v $(pwd)/src:/app/src \
+    -v $(pwd)/.cargo-cache:/usr/local/cargo/registry \
+    -v $(pwd)/target:/app/target \
+    -v $(pwd)/Cargo.toml:/app/Cargo.toml \
+    -v $(pwd)/Cargo.lock:/app/Cargo.lock \
+    --entrypoint bash \
+    rustlang/rust:nightly \
+    -c "cd /app && cargo build"
 
-    mv target/debug/stovoy-tech target/stovoy-tech
-    \cp -f resources/nginx-plain.conf resources/nginx.conf
-) &
-
-(cd static && yarn build) &
-
-wait
+mv target/debug/stovoy-tech target/stovoy-tech
+\cp -f resources/nginx-plain.conf resources/nginx.conf
 
 mkdir -p static/dist
 docker build -t stovoy.tech .
